9efcab7c06238f3a4145230e93af3f896f3315e1
ayowayne123 <ayowayne123@gmail.com>
2022-10-03 11:08:37 +0300

    fixed deployment issues

--- components/ShopHeader.jsx
+++ components/ShopHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import Shop_Header from '../public/Shop_Header.jpg'
+import Shop_header from '../public/Shop_header.jpg'
 
 const ShopHeader = () => {
   return (
     <div className='h-[80vh] overflow-hidden bg-[#00000084]'>
         <div className='  overflow-hidden h-full w-full relative'>
-            <Image className=' mix-blend-overlay' src={Shop_Header} alt='Hero Image'  layout='fill' objectFit='cover'/>
+            <Image className=' mix-blend-overlay' src={Shop_header} alt='Hero Image'  layout='fill' objectFit='cover'/>
             <div className=' absolute pt-16 text-white top-0 left-0 justify-center flex flex-col h-full w-full items-center '> 
             <span className='text-4xl md:text-7xl p-4'>Welcome to CIUCF Shop</span>
             
